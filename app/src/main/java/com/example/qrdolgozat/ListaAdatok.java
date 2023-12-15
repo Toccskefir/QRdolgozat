c4c4a7e8e78d493f34098ea343ad235711280a34
Tóth Kevin <tothkevin0716@gmail.com>
2023-12-15 08:42:34 +0100

    Layout of ListaAdatok view

---
+++ app/src/main/java/com/example/qrdolgozat/ListaAdatok.java
@@ -0,0 +1,14 @@
+package com.example.qrdolgozat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaAdatok extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_adatok);
+    }
+}
