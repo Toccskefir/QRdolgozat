9d8b028e8b79991643da2aeac28fa15f0ca3b319
Tóth Kevin <tothkevin0716@gmail.com>
2023-12-15 09:06:20 +0100

    Created navigation between views and scan

--- app/src/main/java/com/example/qrdolgozat/MainActivity.java
+++ app/src/main/java/com/example/qrdolgozat/MainActivity.java
@@ -1,25 +1,71 @@
 package com.example.qrdolgozat;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
     private Button buttonScan;
     private Button buttonList;
-    private TextView textView;
+    private TextView textViewQRCodeData;
 
     public void init() {
         buttonScan = findViewById(R.id.buttonScan);
         buttonList = findViewById(R.id.buttonList);
-        textView = findViewById(R.id.textView);
+        textViewQRCodeData = findViewById(R.id.textViewQRCodeData);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(
+                requestCode, resultCode, data);
+
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Kilépett a QRCode olvasóból",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                textViewQRCodeData.setText(result.getContents());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+                integrator.setPrompt("QRCode Scan");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.initiateScan();
+            }
+        });
+
+        buttonList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListaAdatok.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
