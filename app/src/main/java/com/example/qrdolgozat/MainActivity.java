1946826faf4a31e7a738150a73bd8b7e45b998dc
Tóth Kevin <tothkevin0716@gmail.com>
2023-12-15 08:25:23 +0100

    Added init to MainActivity.java

--- app/src/main/java/com/example/qrdolgozat/MainActivity.java
+++ app/src/main/java/com/example/qrdolgozat/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.qrdolgozat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonScan;
+    private Button buttonList;
+    private TextView textView;
 
+    public void init() {
+        buttonScan = findViewById(R.id.buttonScan);
+        buttonList = findViewById(R.id.buttonList);
+        textView = findViewById(R.id.textView);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
 }
