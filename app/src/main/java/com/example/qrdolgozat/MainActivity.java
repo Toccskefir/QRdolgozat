efc906678afe9f61aa7d6023d5e168c32ad19305
Tóth Kevin <tothkevin0716@gmail.com>
2023-12-15 09:09:20 +0100

    Put QRCode data into SharedPreferences

--- app/src/main/java/com/example/qrdolgozat/MainActivity.java
+++ app/src/main/java/com/example/qrdolgozat/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.qrdolgozat;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -34,6 +36,11 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "Kilépett a QRCode olvasóból",
                         Toast.LENGTH_SHORT).show();
             } else {
+                SharedPreferences sharedPreferences = getSharedPreferences("Data",
+                        Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("adat", result.getContents());
+                editor.apply();
                 textViewQRCodeData.setText(result.getContents());
             }
         } else {
